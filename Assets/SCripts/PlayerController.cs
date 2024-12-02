e3d007d16bf4d86b7648219f25381f6f6f8992c2
c0smic3atr <marsdatrandomdemon@gmail.com>
2024-12-02 10:18:45 -0800

    Player movement and rotation

--- Assets/SCripts/PlayerController.cs
+++ Assets/SCripts/PlayerController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rb;
+
+    [Header("Movement")]
+    private float verticalInput;
+    private float horizontalInput;
+    public float moveSpeed;
+    public float turnSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Moving
+        verticalInput = Input.GetAxis("Vertical");
+        rb.AddRelativeForce(Vector3.forward * moveSpeed * verticalInput);
+
+        //Turning
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up * turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
